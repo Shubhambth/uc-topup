5af739882d9b1e72c5e5a0451d7d046505d38155
Shubhambth <mixmemebth@gmail.com>
2026-02-05 11:12:29 +0530

    v2

--- qrdata/admin.py
+++ qrdata/admin.py
@@ -1,5 +1,24 @@
+# admin.py
 from django.contrib import admin
-from .models import PaymentQR
+from qrdata.models import PaymentQR
 
 
-admin.site.register(PaymentQR)
+@admin.register(PaymentQR)
+class PaymentQRAdmin(admin.ModelAdmin):
+    list_display = ['package_name', 'package_amount', 'is_active', 'created_at']
+    list_filter = ['is_active', 'created_at']
+    search_fields = ['package_name', 'package_amount']
+    list_editable = ['is_active']
+    ordering = ['package_amount']
+    
+    fieldsets = (
+        ('Package Information', {
+            'fields': ('name', 'package_name', 'package_amount')
+        }),
+        ('QR Code', {
+            'fields': ('qr_image',)
+        }),
+        ('Status', {
+            'fields': ('is_active',)
+        }),
+    )
